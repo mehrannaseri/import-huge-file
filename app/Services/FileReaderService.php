4c04e3183d15ba034a49400832ddf5df2269a7cb
mehran <mehran.naseri2013@gmail.com>
2023-02-14 11:38:34 +0330

    read log data at 500 lines

--- app/Services/FileReaderService.php
+++ app/Services/FileReaderService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use Exception;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
@@ -21,24 +22,39 @@ class FileReaderService
      */
     private bool $divisible;
 
+
+    /**
+     * @var int
+     */
+    private int $limitter;
+
     public function __construct()
     {
         $this->counter = 0;
         $this->divisible = false;
+        $this->limitter = 500;
     }
 
+    /**
+     * get and read file
+     * @return \Generator
+     * @throws Exception
+     */
     public function readFile()
     {
-
+        $file_path = storage_path('app/public/logs.txt');
+        if(! file_exists($file_path)){
+            throw new Exception('file does not exists');
+        }
         $data = [];
-        $file = fopen(storage_path('app/public/logs.txt') , 'r');
+        $file = fopen( $file_path, 'r');
 
         while(!feof($file)) {
             $this->divisible = false;
             $row = fgets($file);
             $this->counter++;
             $data[] =  $row;
-            if($this->counter != 0 && $this->counter % 15 == 0){
+            if($this->counter != 0 && $this->counter % $this->limitter == 0){
                 $this->divisible = true;
                 $chunk = $data;
                 $data = [];
