929f9b57fd6fe4f86e57a01a531f496213fd679d
mehran <mehran.naseri2013@gmail.com>
2023-02-14 10:45:01 +0330

    add log table migration with log model

---
+++ database/migrations/2023_02_14_070442_create_logs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('service_name', 64);
+            $table->smallInteger('status_code');
+            $table->string('uri', 32);
+            $table->string('method',8);
+            $table->string('http_version', 8);
+            $table->dateTime('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+};
